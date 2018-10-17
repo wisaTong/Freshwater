69ea6e23b9b3ae2d23e5d789ffb588fc53635fad
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2018-10-17 22:35:30 +0700

    add hyperlink

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import SendMessage from "./components/SendMessage";
 import RoomList from "./components/RoomList";
 import CreateRoom from "./components/CreateRoom";
 import FriendList from "./components/FriendList";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import Route from "react-router-dom/Route";
 
 const TEMP = [
   {
@@ -27,13 +29,37 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
-        <RoomList />
-        <FriendList />
-        <MessageList messages={this.state.messages} />
-        <SendMessage />
-        <CreateRoom />
-      </div>
+      <Router>
+        <div>
+          <Route
+            path="/"
+            exact
+            render={() => {
+              return (
+                <div>
+                  <h1> Home page </h1>
+                  <Link to="/public-room">PUBLIC ROOM</Link>
+                </div>
+              );
+            }}
+          />
+          <Route
+            path="/public-room"
+            exact
+            render={() => {
+              return (
+                <div className="app">
+                  <RoomList />
+                  <FriendList />
+                  <MessageList messages={this.state.messages} />
+                  <SendMessage />
+                  <CreateRoom />
+                </div>
+              );
+            }}
+          />
+        </div>
+      </Router>
     );
   }
 }
--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,20 +1,6 @@
 import React from "react";
-import {
-  ThemeProvider,
-  AgentBar,
-  Avatar,
-  Column,
-  Title,
-  Subtitle
-} from "@livechat/ui-kit";
 
 class FriendList extends React.Component {
-  theme = {
-    Avatar: {
-      css: {},
-      size: "100px"
-    }
-  };
   render() {
     return (
       <div className="friend-list">
