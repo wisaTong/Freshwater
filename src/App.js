648ea18a30b46d300c9a7c616c62620b85f4a189
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2018-10-17 22:54:27 +0700

    redirect files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./styles/App.css";
 import MessageList from "./components/MessageList";
 import SendMessage from "./components/SendMessage";
 import RoomList from "./components/RoomList";
