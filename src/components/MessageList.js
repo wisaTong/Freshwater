ff6aa3f30935f2db4885a60f01541d999320a818
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2018-10-26 22:14:25 +0700

    add date and time

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -4,11 +4,15 @@ export default class MessageList extends React.Component {
   render() {
     return (
       <div className="message-list">
+        <div className="date">
+          <div className="date-text"> TODAY </div>
+        </div>
         {this.props.messages.map((message, index) => {
           return (
             <div key={index} className="message">
               <div className="message-username">{message.sender}</div>
               <div className="message-text">{message.text}</div>
+              <div className="message-time">{message.time}</div>
             </div>
           );
         })}
