ede3a25c485451b9a17c16148ac54fc6f5230e79
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2018-10-27 20:06:35 +0700

    add MyMessageBubble.js

---
+++ src/components/MyMessageBubble.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class MessageBubble extends React.Component {
+  render() {
+    return (
+      <div className="message">
+        <div className="message-time-me">{this.props.time}</div>
+        <div className="message-text-me">{this.props.text}</div>
+      </div>
+    );
+  }
+}
