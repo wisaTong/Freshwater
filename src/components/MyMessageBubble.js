e0815fd7977d06e51d4fb1e6b0c644941a2184d5
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2018-10-30 19:59:12 +0700

    move message to the left

--- src/components/MyMessageBubble.js
+++ src/components/MyMessageBubble.js
@@ -4,8 +4,8 @@ export default class MessageBubble extends React.Component {
   render() {
     return (
       <div className="message">
-        <div className="message-time-me">{this.props.time}</div>
         <div className="message-text-me">{this.props.text}</div>
+        <div className="message-time-me">{this.props.time}</div>
       </div>
     );
   }
