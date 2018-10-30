342d25b4b3fa3f3ac05b46401a53c81b4b733358
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2018-10-30 13:21:39 +0700

    create constant

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import PublicRoom from "./PublicRoom";
 import { BrowserRouter as Router } from "react-router-dom";
 import Route from "react-router-dom/Route";
 
-ReactDOM.render(
+const Routing = (
   <Router>
     <div>
       <Route path="/" exact component={HomePage} />
       <Route path="/public-room" exact component={PublicRoom} />
     </div>
-  </Router>,
-  document.getElementById("root")
+  </Router>
 );
+
+ReactDOM.render(Routing, document.getElementById("root"));
