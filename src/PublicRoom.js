044af8df0fa2c49884830e8a64cfa50016d483ac
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2018-10-30 19:58:32 +0700

    add new message

--- src/PublicRoom.js
+++ src/PublicRoom.js
@@ -8,8 +8,17 @@ import SendMessage from "./components/SendMessage";
 import { Message } from "./components/Message";
 
 const TEMP = [
+  new Message(
+    "me",
+    "HelloWorld Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, ratione fugit. Atque commodi autem velit sit est dolores aut dignissimos, tene",
+    "15:30"
+  ),
   new Message("me", "HelloWorld", "15:30"),
-  new Message("ZZZZZZ", "asdsadads", "15:30"),
+  new Message(
+    "ZZZZZZ",
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, ratione fugit. Atque commodi autem velit sit est dolores aut dignissimos, tenetur, impedit quaerat quo odio aliquam optio reiciendis, in id?",
+    "15:30"
+  ),
   new Message("ZZZZZZ", "asdsadads", "15:30"),
   new Message("ZZZZZZ", "asdsadads", "15:30"),
   new Message("ZZZZZZ", "asdsadads", "15:30"),
