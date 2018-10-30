35fbeb5f728759cba9ec02b060ed38129bd2a099
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2018-10-30 10:45:47 +0700

    set state for username

--- src/PublicRoom.js
+++ src/PublicRoom.js
@@ -7,32 +7,19 @@ import SendMessage from "./components/SendMessage";
 
 import { Message } from "./components/Message";
 
-var firstSender = new Message("First", "HelloWorld", "15:30");
-var secondSender = new Message("ZZZZZZ", "asdsadads", "15:30");
-var secondSender1 = new Message("ZZZZZZ", "asdsadads", "15:30");
-var secondSender2 = new Message("ZZZZZZ", "asdsadads", "15:30");
-var secondSender3 = new Message("ZZZZZZ", "asdsadads", "15:30");
-var secondSender4 = new Message("ZZZZZZ", "asdsadads", "15:30");
-var secondSender5 = new Message("ZZZZZZ", "asdsadads", "15:30");
-var secondSender6 = new Message("ZZZZZZ", "asdsadads", "15:30");
-var secondSender7 = new Message("ZZZZZZ", "asdsadads", "15:30");
-var secondSender8 = new Message("ZZZZZZ", "asdsadads", "15:30");
-var secondSender9 = new Message("ZZZZZZ", "asdsadads", "15:30");
-var secondSender10 = new Message("ZZZZZZ", "asdsadads", "15:30");
-
 const TEMP = [
-  firstSender,
-  secondSender,
-  secondSender1,
-  secondSender2,
-  secondSender3,
-  secondSender4,
-  secondSender5,
-  secondSender6,
-  secondSender7,
-  secondSender8,
-  secondSender9,
-  secondSender10
+  new Message("me", "HelloWorld", "15:30"),
+  new Message("ZZZZZZ", "asdsadads", "15:30"),
+  new Message("ZZZZZZ", "asdsadads", "15:30"),
+  new Message("ZZZZZZ", "asdsadads", "15:30"),
+  new Message("ZZZZZZ", "asdsadads", "15:30"),
+  new Message("ZZZZZZ", "asdsadads", "15:30"),
+  new Message("ZZZZZZ", "asdsadads", "15:30"),
+  new Message("ZZZZZZ", "asdsadads", "15:30"),
+  new Message("ZZZZZZ", "asdsadads", "15:30"),
+  new Message("ZZZZZZ", "asdsadads", "15:30"),
+  new Message("ZZZZZZ", "asdsadads", "15:30"),
+  new Message("ZZZZZZ", "asdsadads", "15:30")
 ];
 
 export default class PublicRoom extends React.Component {
