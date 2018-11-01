15e760ade2abb15912a024b6a5e9b61bdb8d2b48
First529 <first_dejabhol@hotmail.com>
2018-11-01 22:36:29 +0700

    change className

--- src/components/MyMessageBubble.js
+++ src/components/MyMessageBubble.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default class MessageBubble extends React.Component {
+export default class MyMessageBubble extends React.Component {
   render() {
     return (
-      <div className="message">
-        <div className="message-text-me">{this.props.text}</div>
+      <div className="message-me">
         <div className="message-time-me">{this.props.time}</div>
+        <div className="message-text-me">{this.props.text}</div>
       </div>
     );
   }
