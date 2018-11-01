3963343bee9b3ad146b8971c84473c2d48f03471
First529 <first_dejabhol@hotmail.com>
2018-11-01 22:40:43 +0700

    remove rectangle-box

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -3,10 +3,8 @@ import React from "react";
 export default class SendMessage extends React.Component {
   render() {
     return (
-      <div className="rectangle-box">
-        <div className="send-message">
-          <input placeholder="Write your message here" type="text" />
-        </div>
+      <div className="send-message">
+        <input placeholder="Write your message here" type="text" />
       </div>
     );
   }
