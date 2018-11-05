3f2ef6be3a15e5eebbfc62604dc6aa0a1b9eeb8b
Wisa Powthongchin <wisa.pow@gmail.com>
2018-11-05 12:02:51 +0700

    refactored Bubble components

--- src/components/MessageBubble.js
+++ src/components/MessageBubble.js
@@ -3,8 +3,8 @@ import React from "react";
 export default class MessageBubble extends React.Component {
   render() {
     return (
-      <div className="message">
-        <div className="message-text">{this.props.text}</div>
+      <div>
+        <div className="message-bubble">{this.props.text}</div>
         <div className="message-time">{this.props.time}</div>
       </div>
     );
--- src/components/MyMessageBubble.js
+++ src/components/MyMessageBubble.js
@@ -3,9 +3,9 @@ import React from "react";
 export default class MyMessageBubble extends React.Component {
   render() {
     return (
-      <div className="message-me">
-        <div className="message-time-me">{this.props.time}</div>
-        <div className="message-text-me">{this.props.text}</div>
+      <div className="my-message-slot">
+        <div className="my-message-time">{this.props.time}</div>
+        <div className="my-message-bubble">{this.props.text}</div>
       </div>
     );
   }
