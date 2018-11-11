a54a80358fce493ed8e3d382df636958991158df
booknap <napasai45@gmail.com>
2018-11-11 22:18:52 +0700

    connected websocket

--- src/components/PublicRoom.js
+++ src/components/PublicRoom.js
@@ -6,6 +6,7 @@ import MessageList from "./MessageList";
 import SendMessage from "./SendMessage";
 
 import { Message } from "./Message";
+import* as ws from "../socketClient"
 
 const TEMP = [
   new Message(
@@ -25,6 +26,15 @@ const TEMP = [
 export default class PublicRoom extends React.Component {
   constructor() {
     super();
+
+    let sock = ws.connect("ws://35.240.212.170/ws")
+    
+    sock.onmessage = (msg) => {
+      console.log(`[info] receive message: ${msg.data}`);
+      TEMP.push(new Message("Book", msg.data, "10.10"));
+      this.setState({messages: TEMP});
+    }
+
     this.state = {
       name: "Public"
       , messages: TEMP
