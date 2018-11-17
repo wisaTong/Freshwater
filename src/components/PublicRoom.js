0526c6093ba25ee314f1d867b3b4e55c82955944
booknap <napasai45@gmail.com>
2018-11-18 00:02:09 +0700

    send message to public room

--- src/components/PublicRoom.js
+++ src/components/PublicRoom.js
@@ -11,43 +11,48 @@ import* as ws from "../socketClient"
 const TEMP = [
   new Message(
     "me",
+    "public",
     "HelloWorld Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, ratione fugit. Atque commodi autem velit sit est dolores aut dignissimos, tene",
     "15:30"
   ),
-  new Message("me", "HelloWorld", "15:30"),
+  new Message("me", "public", "HelloWorld", "15:30"),
   new Message(
     "Mr.Lorem",
+    "public",
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, ratione fugit. Atque commodi autem velit sit est dolores aut dignissimos, tenetur, impedit quaerat quo odio aliquam optio reiciendis, in id?",
     "15:30"
   ),
-  new Message("Classic man", "asdsadads", "15.35")
+  new Message("Classic man", "public", "asdsadads", "15.35")
 ];
-
+let sock = ws.connect("ws://35.240.212.170/ws");
 export default class PublicRoom extends React.Component {
   constructor() {
     super();
-
-    let sock = ws.connect("ws://35.240.212.170/ws")
-    
+    this.state = {
+      name: "Public",
+      messages: TEMP
+    };
     sock.onmessage = (msg) => {
       let some = JSON.parse(msg.data)
       var time = new Date();
       console.log(`[info] receive message: ${msg.data}`);
-      TEMP.push(new Message(some.sender, some.message, time.getHours()+":"+time.getMinutes()));
+      TEMP.push(new Message(some.sender, some.destination ,some.message, time.getHours()+":"+time.getMinutes()));
       this.setState({messages: TEMP});
-    }
-
-    this.state = {
-      name: "Public"
-      , messages: TEMP
     };
+    this.sendMessageToSocket = this.sendMessageToSocket.bind(this);
   }
+
+  sendMessageToSocket(message) {
+    let msg = JSON.stringify(new Message("me","public",message,"9000"));
+    sock.send(msg);
+  }
+
   render() {
     return (
       <div className="chat-box">
         <div className="title-font"> {this.state.name} </div>
         <MessageList messages={this.state.messages} />
-        <SendMessage />
+        <SendMessage sendMessage={(message) => this.sendMessageToSocket(message)}/>
       </div>
     );
   }
--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,10 +1,28 @@
 import React from "react";
 
 export default class SendMessage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { message: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({message:e.target.value})
+  }
+
+  handleKeyPress = (event) => {
+    if(event.key === 'Enter'){
+      this.props.sendMessage(this.state.message);
+      this.setState({message: ''});
+    }
+  }
+
   render() {
     return (
       <div className="send-message">
-        <input placeholder="Write your message here" type="text" />
+        <input placeholder="Write your message here" value={this.state.message} type="message" onChange={this.handleChange} onKeyPress={this.handleKeyPress}/>
       </div>
     );
   }
