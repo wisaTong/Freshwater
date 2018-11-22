74f80c68683fd563a7529aa7d29c13e17e7dd379
First529 <first_dejabhol@hotmail.com>
2018-11-22 16:28:46 +0700

    add link to public room

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,10 +1,33 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import ReactDOM from "react-dom";
+
 import "../styles/login.css";
 
 import google_logo from "../images/google_logo.svg"
 import facebook_logo from "../images/facebook_logo.svg"
+import PublicRoom from "./PublicRoom";
 
 export default class LoginPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { username: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({username:event.target.value})
+  }
+
+  handleKeyPress = (event) => {
+    if(event.key === 'Enter' && this.state.username.trim() !== ''){
+      console.log(this.state.username);
+      ReactDOM.render(<PublicRoom username={this.state.username}/>, document.getElementById('root'));
+    }
+  }
+
   render() {
     return (
       <div className="container">
@@ -15,24 +38,26 @@ export default class LoginPage extends React.Component {
           <h3>A simple chat app</h3>
           <div className="send-name">
             {" "}
-            <input placeholder="Enter your name" type="text" />{" "}
+            <input placeholder="Enter your name" type="text" value={this.state.username} onChange={this.handleChange} onKeyPress={this.handleKeyPress}/>{" "}
           </div>
           <p> or </p>
-          <button className="container-google-color">
-            <img src={google_logo} alt={"google_logo"}/>
-            <div className="container-google-text">
-              {" "}
-              Sign in with Google account{" "}
-            </div>
-          </button>
-          <div className="empty-space" />
-          <button className="container-facebook-color">
-            <img src={facebook_logo} alt={"facebook_logo"}/>
-            <div className="container-facebook-text">
-              {" "}
-              Sign in with Facebook account{" "}
-            </div>
-          </button>
+          <Link to="/public-room">
+            <button className="container-google-color">
+              <img src={google_logo} alt={"google_logo"}/>
+              <div className="container-google-text">
+                {" "}
+                Sign in with Google account{" "}
+              </div>
+            </button>
+            <div className="empty-space" />
+            <button className="container-facebook-color">
+              <img src={facebook_logo} alt={"facebook_logo"}/>
+              <div className="container-facebook-text">
+                {" "}
+                Sign in with Facebook account{" "}
+              </div>
+            </button>
+          </Link>
         </div>
       </div>
     );
