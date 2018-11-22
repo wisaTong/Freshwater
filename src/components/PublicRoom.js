5d82a21f7ccbd0b9ebe1177ae0b0038401b132d3
booknap <napasai45@gmail.com>
2018-11-22 16:55:49 +0700

    fixed bug and warning

--- src/components/PublicRoom.js
+++ src/components/PublicRoom.js
@@ -8,36 +8,23 @@ import SendMessage from "./SendMessage";
 import { Message } from "./Message";
 import * as ws from "../socketClient"
 
-const TEMP = [
-  new Message(
-    "me",
-    "public",
-    "HelloWorld Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, ratione fugit. Atque commodi autem velit sit est dolores aut dignissimos, tene",
-    "15:30"
-  ),
-  new Message("me", "public", "HelloWorld", "15:30"),
-  new Message(
-    "Mr.Lorem",
-    "public",
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, ratione fugit. Atque commodi autem velit sit est dolores aut dignissimos, tenetur, impedit quaerat quo odio aliquam optio reiciendis, in id?",
-    "15:30"
-  ),
-  new Message("Classic man", "public", "asdsadads", "15.35")
-];
-let sock = ws.connect("ws://35.240.212.170/ws");
+let sock;
+
 export default class PublicRoom extends React.Component {
   constructor() {
+    sock = ws.connect("ws://35.240.212.170/ws");
+
     super();
     this.state = {
       name: "Public",
-      messages: TEMP
+      messages: []
     };
     sock.onmessage = (msg) => {
       let some = JSON.parse(msg.data)
       var time = new Date();
       console.log(`[info] receive message: ${msg.data}`);
-      TEMP.push(new Message(some.sender, some.destination ,some.message, time.getHours()+":"+time.getMinutes()));
-      this.setState({messages: TEMP});
+      this.state.messages.push(new Message(some.sender, some.destination ,some.message, time.getHours()+":"+time.getMinutes()));
+      this.setState({messages: this.state.messages});
     };
     this.sendMessageToSocket = this.sendMessageToSocket.bind(this);
   }
