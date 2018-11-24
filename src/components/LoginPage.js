afe97a744dc6a660a3decd7f5679e16fccf37451
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2018-11-24 21:27:19 +0700

    try removing render

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,32 +1,30 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import ReactDOM from "react-dom";
 
 import "../styles/login.css";
 
-import google_logo from "../images/google_logo.svg"
-import facebook_logo from "../images/facebook_logo.svg"
+import google_logo from "../images/google_logo.svg";
+import facebook_logo from "../images/facebook_logo.svg";
 import PublicRoom from "./PublicRoom";
 
 export default class LoginPage extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = { username: '' };
+    this.state = { username: "" };
     this.handleChange = this.handleChange.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   handleChange(event) {
-    this.setState({username:event.target.value})
+    this.setState({ username: event.target.value });
   }
 
-  handleKeyPress = (event) => {
-    if(event.key === 'Enter' && this.state.username.trim() !== ''){
+  handleKeyPress = event => {
+    if (event.key === "Enter" && this.state.username.trim() !== "") {
       console.log(this.state.username);
-      ReactDOM.render(<PublicRoom username={this.state.username}/>, document.getElementById('root'));
     }
-  }
+  };
 
   render() {
     return (
@@ -38,12 +36,19 @@ export default class LoginPage extends React.Component {
           <h3>A simple chat app</h3>
           <div className="send-name">
             {" "}
-            <input placeholder="Enter your name" type="text" value={this.state.username} onChange={this.handleChange} onKeyPress={this.handleKeyPress}/>{" "}
+            <input
+              placeholder="Enter your name"
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+              onKeyPress={this.handleKeyPress}
+            />{" "}
           </div>
           <p> or </p>
+
           <Link to="/public-room">
             <button className="container-google-color">
-              <img src={google_logo} alt={"google_logo"}/>
+              <img src={google_logo} alt={"google_logo"} />
               <div className="container-google-text">
                 {" "}
                 Sign in with Google account{" "}
@@ -51,7 +56,7 @@ export default class LoginPage extends React.Component {
             </button>
             <div className="empty-space" />
             <button className="container-facebook-color">
-              <img src={facebook_logo} alt={"facebook_logo"}/>
+              <img src={facebook_logo} alt={"facebook_logo"} />
               <div className="container-facebook-text">
                 {" "}
                 Sign in with Facebook account{" "}
