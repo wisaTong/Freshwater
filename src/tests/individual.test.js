34aea4b923f0441cf85ecb92969e77d437444610
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2018-11-24 21:00:20 +0700

    update test

--- src/tests/individual.test.js
+++ src/tests/individual.test.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Loginpage test
-import LoginPage from '../components/LoginPage'
+import LoginPage from "../components/LoginPage";
 
-it('[LoginPage] renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<LoginPage />, div);
+it("[LoginPage] renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<LoginPage username={"test"} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 // MessageBubble test
-import MessageBubble from '../components/MessageBubble'
+import MessageBubble from "../components/MessageBubble";
 
-it('[MessageBubble] renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MessageBubble text={'Placeholder'} time={'15:30'}/>, div);
+it("[MessageBubble] renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<MessageBubble text={"Placeholder"} time={"15:30"} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 // MyMessageBubble test
-import MyMessageBubble from '../components/MyMessageBubble'
+import MyMessageBubble from "../components/MyMessageBubble";
 
-it('[MyMessageBubble] renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MyMessageBubble text={'Placeholder'} time={'15:30'}/>, div);
+it("[MyMessageBubble] renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<MyMessageBubble text={"Placeholder"} time={"15:30"} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
-
