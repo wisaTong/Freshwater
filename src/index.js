03376bf868a33d489bc1294bbfdc8f2eaedb77ee
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2018-11-27 17:57:52 +0700

    add Switch

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Route from "react-router-dom/Route";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
 
 import HomePage from "./components/HomePage";
 import PublicRoom from "./components/PublicRoom";
 import LoginPage from "./components/LoginPage";
 
-
 const Routing = (
-  <Router>
-    <div>
+  <BrowserRouter>
+    <Switch>
       <Route path="/" exact component={HomePage} />
       <Route path="/public-room" exact component={PublicRoom} />
       <Route path="/login" exact component={LoginPage} />
-    </div>
-  </Router>
+    </Switch>
+  </BrowserRouter>
 );
 
 ReactDOM.render(Routing, document.getElementById("root"));
