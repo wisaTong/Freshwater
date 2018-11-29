277f87d5b32e663945f6c181da7f8c39d740605f
First529 <first_dejabhol@hotmail.com>
2018-11-29 13:55:58 +0700

    set id to Link

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/login.css";
-import { Link, BrowserRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import google_logo from "../images/google_logo.svg";
 import facebook_logo from "../images/facebook_logo.svg";
@@ -17,7 +17,8 @@ export default class LoginPage extends React.Component {
   }
   handleKeyPress = event => {
     if (event.key === "Enter" && this.state.username.trim() !== "") {
-      console.log(this.state.username);
+      document.getElementById("submit").click();
+      return console.log(`Your username is : ${this.state.username}`)
     }
   };
 
@@ -40,30 +41,27 @@ export default class LoginPage extends React.Component {
             />{" "}
           </div>
           <p> or </p>
-          <BrowserRouter>
+            <button className="container-google-color">
+              <img src={google_logo} alt={"google_logo"} />
+              <div className="container-google-text">
+                {" "}
+                Sign in with Google account{" "}
+              </div>
+            </button>
+            <div className="empty-space" />
+            <button className="container-facebook-color">
+              <img src={facebook_logo} alt={"facebook_logo"} />
+              <div className="container-facebook-text">
+                {" "}
+                Sign in with Facebook account{" "}
+              </div>
+            </button>
             <Link
               to={{
                 pathname: "/public-room",
                 state: { fromUserName: this.state.username }
               }}
-            >
-              <button className="container-google-color">
-                <img src={google_logo} alt={"google_logo"} />
-                <div className="container-google-text">
-                  {" "}
-                  Sign in with Google account{" "}
-                </div>
-              </button>
-              <div className="empty-space" />
-              <button className="container-facebook-color">
-                <img src={facebook_logo} alt={"facebook_logo"} />
-                <div className="container-facebook-text">
-                  {" "}
-                  Sign in with Facebook account{" "}
-                </div>
-              </button>
-            </Link>
-          </BrowserRouter>
+            id="submit"/>
         </div>
       </div>
     );
