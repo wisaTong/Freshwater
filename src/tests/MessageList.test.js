9b0c20b1dd1b9ed610e5e9d64013615c762ed5fc
Wisa Powthongchin <wisa.pow@gmail.com>
2018-12-06 05:39:55 +0700

    fixed test suite

--- src/tests/MessageList.test.js
+++ src/tests/MessageList.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import MessageList from "../components/MessageList";
-import { Message } from "../components/Message";
+import Message from "../components/Message";
 import MyMessageBubble from "../components/MyMessageBubble";
 
 const TEMP = [
